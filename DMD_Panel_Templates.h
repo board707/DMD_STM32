52a3f35f9afcd9abeed8790bde96b505ae768b4f
board707 <ddt@jwee.ru>
2023-06-30 01:14:27 +0300

    Add 80x40 s10 panel template

--- DMD_Panel_Templates.h
+++ DMD_Panel_Templates.h
@@ -59,7 +59,8 @@
 #define RGB32x16_S2_quangli		2,32,16,2,53	// 32x16 1/2 complex pattern, DIRECT mux
 #define RGB32x16_S2_horro		1,32,16,2,54	// 32x16 1/2 complex pattern, BINARY mux from @horro
 #define RGB32x16_S2_OKSingra	1,32,16,2,55	// 32x16 1/2 complex pattern, BINARY mux from @OKSingra
-#define RGB40x20_S5_Inikon		3,40,20,5,56	// 40x20 1/5 complex pattern, BINARY mux from @Inikon
+#define RGB40x20_S5_LNikon		3,40,20,5,56	// 40x20 1/5 4pixels pattern, BINARY mux from @LNikon
+#define RGB80x40_S10_LNikon     4,80,40,10,57	// 80x40 1/10 4pixels pattern, BINARY mux from @LNikon
 
 
 
@@ -271,14 +272,15 @@ protected:
 	}
 };
 //--------------------------------------------------------------------------------------
-// 40x20 1/5 matrix from Inikon with 4-pixel pattern
+// 40x20 1/5 matrix from LNikon 
 // BINARY mux
 // 138 mux, SN16207 driver
 //
+// 4-pixel pattern, lower line first (LPS type)
 // Qiang li Q8S5V3H BQWZ-ZP
 //--------------------------------------------------------------------------------------/
 template<int COL_DEPTH>
-class DMD_RGB<RGB40x20_S5_Inikon, COL_DEPTH> : public DMD_RGB_BASE2<COL_DEPTH>
+class DMD_RGB<RGB40x20_S5_LNikon, COL_DEPTH> : public DMD_RGB_BASE2<COL_DEPTH>
 	{
 	public:
 		DMD_RGB(uint8_t* mux_list, byte _pin_nOE, byte _pin_SCLK, uint8_t* pinlist,
@@ -300,6 +302,43 @@ class DMD_RGB<RGB40x20_S5_Inikon, COL_DEPTH> : public DMD_RGB_BASE2<COL_DEPTH>
 			uint16_t base_addr = (pol_y % this->nRows) * this->x_len + (x / 4) * this->multiplex * 4;
 			if (pol_y / this->nRows) base_addr += x % 4;
 			else base_addr += (4 + x % 4);
+			return base_addr;
+			}
+	};
+
+//--------------------------------------------------------------------------------------
+// 80x40 1/10 matrix from LNikon 
+// BINARY mux
+// SM5166P mux, SM16227S driver
+//
+// 4-pixel pattern, upper line first (UPS type)
+// Qiang li Q4Y10V5H
+//--------------------------------------------------------------------------------------/
+template<int COL_DEPTH>
+class DMD_RGB< RGB80x40_S10_LNikon, COL_DEPTH> : public DMD_RGB_BASE2<COL_DEPTH>
+	{
+	public:
+		DMD_RGB(uint8_t* mux_list, byte _pin_nOE, byte _pin_SCLK, uint8_t* pinlist,
+			byte panelsWide, byte panelsHigh, bool d_buf = false) :
+			DMD_RGB_BASE2<COL_DEPTH>(4, mux_list, _pin_nOE, _pin_SCLK, pinlist,
+				panelsWide, panelsHigh, d_buf, COL_DEPTH, 10, 80, 40)
+			{
+			this->fast_Hbyte = false;
+			this->use_shift = false;
+			}
+
+		// Fast text shift is disabled for complex patterns, so we don't need the method
+		void disableFastTextShift(bool shift) override {}
+
+	protected:
+
+		uint16_t get_base_addr(int16_t& x, int16_t& y) override {
+			this->transform_XY(x, y);
+			uint8_t pol_y = y % this->pol_displ;
+			x += (y / this->DMD_PIXELS_DOWN) * this->WIDTH;
+			uint16_t base_addr = (pol_y % this->nRows) * this->x_len + (x / 4) * this->multiplex * 4;
+			if (pol_y / this->nRows) base_addr += (4 + x % 4);
+			else  base_addr += x % 4;
 			return base_addr;
 			}
 	};
