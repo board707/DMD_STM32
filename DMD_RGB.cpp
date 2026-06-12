903beeef5f6f01f88c9b83ddd93f5b6523594c7a
board707 <50185434+board707@users.noreply.github.com>
2026-06-12 03:21:29 +0300

    Update DMD_RGB.cpp
    Remove flushPanel... and PanelResync methods. Tested.

--- DMD_RGB.cpp
+++ DMD_RGB.cpp
@@ -154,20 +154,7 @@ void DMD_RGB_BASE::initialize_timers(voidFuncPtr handler) {
 }
 #endif
 
-/*--------------------------------------------------------------------------------------*/
-#if (defined(__STM32F1__) || defined(__STM32F4__))
-void DMD_RGB_BASE::flushPanelShiftRegs() {
-	for (uint8_t pass = 0; pass < 4; pass++) {
-		for (uint8_t row = 0; row < nRows; row++) {
-			Mux->set_mux(row);
-			send_to_allRGB(0, 3);
-			send_to_allRGB(0, 3);
-		}
-	}
-	Mux->set_mux(0);
-}
 
-void DMD_RGB_BASE::panelResync() {
 }
 #endif
 
@@ -185,9 +172,6 @@ void DMD_RGB_BASE::init(uint16_t user_fps) {
 	set_pin_modes();
 	//generate_muxmask();
 	generate_rgbtable();
-#if defined(DMD_STM32DUINO)
-	flushPanelShiftRegs();
-#endif
 	chip_init();
 #elif (defined(ARDUINO_ARCH_RP2040))
 	// generate_muxmask();
@@ -195,11 +179,7 @@ void DMD_RGB_BASE::init(uint16_t user_fps) {
 	initialize_timers(scan_running_dmd_R);
 	setBrightness(200);
 	clearScreen(true);
-#if defined(DMD_STM32DUINO)
-	plane = 0;
-	row = 0;
-	buffptr = matrixbuff[1 - backindex];
-#endif
+
 
 }
 /*--------------------------------------------------------------------------------------*/
@@ -369,8 +349,7 @@ void DMD_RGB_BASE::scan_dmd_p1() {
 			if (swapflag == true) {    // Swap front/back buffers if requested
 				backindex = 1 - backindex;
 				swapflag = false;
-				bDMDScreenRAM = matrixbuff[backindex];
-				front_buff = matrixbuff[1 - backindex];
+				
 				}
 			}
 		buffptr = matrixbuff[1 - backindex]; // Reset into front buffer
