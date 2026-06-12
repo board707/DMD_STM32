ba6ac630ce27e9f0602a70b30081b49e80d7d5d2
board707 <50185434+board707@users.noreply.github.com>
2026-06-12 03:17:12 +0300

    Update DMD_RGB.h
    Remove flushPanel... and PanelResync methods. Tested.

--- DMD_RGB.h
+++ DMD_RGB.h
@@ -97,7 +97,6 @@ public:
 	}
 
 	void configure_multiplexer(MUX_TYPE mux);
-	void panelResync();
 	
 	~DMD_RGB_BASE();
 
@@ -120,7 +119,7 @@ protected:
 		
 #endif
     virtual void send_to_allRGB(uint16_t data, uint16_t latches);
-    void flushPanelShiftRegs();
+    
     virtual void chip_init() {};
 	virtual void setCycleLen();
 	virtual uint16_t get_base_addr(int16_t& x, int16_t& y);
